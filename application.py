54f7c320a35e6d58fd8bb2fd2d882abe646df1e1
yangliclai <yangliclai@gmail.com>
2018-05-31 00:44:15 -0400

    update to VPS-work-well application.py

--- application.py
+++ application.py
@@ -1,24 +1,28 @@
 from flask import Flask, redirect, url_for
 from flask_mongoengine import MongoEngine
+#import config
 
 db = MongoEngine()
+app = Flask(__name__)
 
-def create_app(config=None):
-    app = Flask(__name__)
-    if config is not None:
-        app.config.from_object(config)
+app.config.from_object('config')
 
-    db.init_app(app)
+db.init_app(app)
 
-    
-    from user.views import user_page
-    app.register_blueprint(user_page,url_prefix="/user")
+from user.views import user_page
+app.register_blueprint(user_page,url_prefix="/user")
 
-    from party.views import party_page
-    app.register_blueprint(party_page,url_prefix="/party")
+from party.views import party_page
+app.register_blueprint(party_page,url_prefix="/party")
 
-    @app.route('/')
-    def home():
-        return redirect(url_for('party_page.explore'))
-    
-    return app
+@app.route('/')
+def home():
+    return redirect(url_for('party_page.explore'))
+
+#def index():
+#    return '<h1>Hello World For Test10!</h1>'
+
+
+if __name__ == '__main__':
+    #app = create_app(config='config')
+    app.run(debug=False)
